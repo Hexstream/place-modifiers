2a3eabe7cdbaaed029aa6a48f1387c6d13906c33
Jean-Philippe Paradis <hexstream@gmail.com>
2012-08-27 15:29:35 -0400

    Almost done with the definitions.

--- definitions.lisp
+++ definitions.lisp
@@ -20,13 +20,12 @@
   (alpha-char-p t)
   (alphanumericp 1)
   ((and or) t (1 -1))
-  ((append nconc) t (-1 1))
+  ((append nconc list*) t (-1 1))
   (apply nil (-1 2 1))
   (funcall nil (2 -1 1))
   (apropos-list t (1 2))
   (aref nil (1 2 -1))
-  (elt nil (1 2))
-  (svref nil (1 2))
+  ((elt svref) nil (1 2))
   ((arithmetic-error-operands arithmetic-error-operation) nil)
   (array-dimension nil (1 2))
   (array-dimensions nil)
@@ -107,10 +106,9 @@
 (place-modifier:define ()
   ((compile-file ; Ignore second and third return values.
     compile-file-pathname
-    compiled-function-p
-    compiler-macro-function) ; Omit pointless secondary variant.
-   t) 
-  (compiler-macro-function nil (1 2)) 
+    compiled-function-p)
+   t)
+  ((compiler-macro-function macro-function) nil) ; Omit pointless secondary variant.
   (complement t)
   (complex t (1 2))
   (complexp t)
@@ -135,6 +133,7 @@
 
   ((find find-if find-if-not
          member member-if member-if-not
+         position position-if position-if-not
          remove remove-if remove-if-not
          delete delete-if delete-if-not
          count count-if count-if-not)
@@ -176,7 +175,7 @@
   (find-method nil)
   (find-package nil)
   (find-restart nil)
-  (find-symbol nil (1 2)) ; Ignore second return value.
+  ((find-symbol intern) nil (1 2)) ; Ignore second return value.
   (float t (1 2))
   ((float-digits float-precision float-radix)
    t)
@@ -186,148 +185,116 @@
   (function-lambda-expression t) ; Ignore second and third return values.
   (functionp t)
   ((gcd lcm) t (1 -1))
-  gensym
-  get ((get *) 2)
-  get-macro-character ((get-macro-character *) 2)
-  get-output-stream-string
-  get-properties (get-properties *)
-  get-setf-expansion
-  getf ((getf *) 2)
-  (gethash 2) ((gethash *) 1)
-  graphic-char-p
-  #+nil(macro handler-case)
-  hash-table-count
-  hash-table-p
-  hash-table-rehash-size
-  hash-table-rehash-threshold
-  hash-table-size
-  hash-table-test
-  identity
-  imagpart
-  input-stream-p
-  integer-decode-float
-  integer-length
-  integerp
-  interactive-stream-p
-  intern ((intern *) 2)
-  intersection ((intersection *) 2)
-  nintersection ((nintersection *) 2)
-  invoke-restart
-  invoke-restart-interactively
-  isqrt
-  keywordp
-  last ((last *) 2)
-  lcm (lcm *)
-  (ldb 2)
-  (ldb-test 2)
-  ldiff ((ldiff *) 2)
-  length
-  list (list *)
-  (list* -1) ((list* *) 1)
-  list-length
-  listen
-  listp
-  load
-  load-logical-pathname-translations
-  log ((log *) 2)
-  logand (logand *)
-  logandc1 (logandc1 *)
-  logandc2 (logandc2 *)
-  (logbitp 2) ((logbitp *) 1)		; caution
-  logcount
-  logeqv (logeqv *)
-  logical-pathname
-  logical-pathname-translations
-  logior (logior *)
-  lognand (lognand *)
-  lognor (lognor *)
-  lognot
-  logorc1
-  logorc2
-  logtest (logtest *)
-  logxor (logxor *)
-  macro-function
-  macroexpand
-  macroexpand-1
-  make-array
-  make-broadcast-stream (make-broadcast-stream *)
-  make-concatenated-stream (make-concatenated-stream *)
-  make-condition
-  make-echo-stream (make-echo-stream *)
-  make-instance
-  make-list
-  make-load-form
-  make-load-form-saving-slots
-  make-package
-  make-random-state
-  (make-sequence 2) ((make-sequence *) 1) ; caution
-  make-string
-  make-string-input-stream
-  make-symbol
-  make-synonym-stream
-  make-two-way-stream (make-two-way-stream *)
-  (map 3) (map *)
-  (map-into 3) (map-into *)
-  (mapc 2) (mapc *)			; caution, todo
-  (mapcar 2) (mapcar *) (mapcan 2) (mapcan *)
-  mapl                                  ; caution, todo
-  (maplist 2) (maplist *) (mapcon 2) (mapcon *)
-  maphash				; caution, todo
-  (maskfield 2)
-  max (max *)
-  (member 2) (member-if 2) (member-if-not 2) ; caution
-  ((member *) 1) ((member-if *) 1) ((member-if-not *) 1)
-  (merge 2) ((merge *) 3)
-  merge-pathnames
-  method-qualifiers
-  min (min *)
-  minusp
-  mismatch ((mismatch *) 2)
-  mod (mod *)
-  multiple-value-call			; caution, todo
-  name-char
-  no-applicable-method
-  no-next-method
-  (nreconc 2) ((nreconc *) 1)
-  nreverse
-  nset-difference ((nset-difference *) 2)
-  nset-exclusive-or ((nset-exclusive-or *) 2)
-  nstring-capitalize
-  nstring-downcase
-  nstring-upcase
+  (gensym t)
+  (get nil (1 2))
+  (get-macro-character nil (1 2))
+  (get-output-stream-string t)
+  ; (get-properties t) (3 return values)
+  ; get-setf-expansion (5 return values)
+  (getf nil (1 2 3))
+  (gethash nil (2 1 3)) ; Ignore second return value.
+  (graphic-char-p nil)
+  ; handler-case
+  ((hash-table-count hash-table-p) t)
+  ((hash-table-rehash-size
+    hash-table-rehash-threshold
+    hash-table-size
+    hash-table-test)
+   nil)
+  (identity t)
+  ((realpart imagpart) nil)
+  (phase t)
+  ((input-stream-p output-stream-p) t)
+  ; (integer-decode-float t) (3 return values)
+  (integer-length t)
+  (integerp t)
+  (interactive-stream-p t)
+  ((intersection set-difference set-exclusive-or union
+                 nintersection nset-difference nset-exclusive-or nunion)
+   t (1 2))
+  (invoke-restart t (1 2 -1)) ; Ignore all but first return value.
+  (invoke-restart-interactively t) ; Ignore all but first return value.
+  ((sqrt isqrt) t)
+  (keywordp t)
+  (last nil (1 2))
+  ((ldb ldb-test mask-field) nil (2 1))
+  (ldiff t (1 2))
+  ((length list-length) t)
+  (list t (1 -1))
+  (listen t)
+  (listp t)
+  (load t)
+  (load-logical-pathname-translations t)
+  (log t (1 2))
+  ((logand lognand logandc1 logandc2 logeqv
+           logior lognor logorc1 logorc2 logxor logtest)
+   t (1 -1))
+  (lognot t)
+  (logbitp t (2 1))
+  (logcount t)
+  (logical-pathname t)
+  (logical-pathname-translations nil)
+  ;; Omit pointless secondary variant and ignore second return value.
+  ((macroexpand macroexpand-1) t)
+  (make-array t)
+  ((make-broadcast-stream make-concatenated-stream) t (1 -1))
+  (make-condition t)
+  (make-echo-stream t (1 2))
+  (make-instance t)
+  (make-list t)
+  ;; Omit pointless secondary variant and ignore second return value.
+  (make-load-form t)
+  (make-load-form-saving-slots t) ; Ignore second return value.
+  (make-package t)
+  (make-random-state t)
+  (make-sequence t (2 1))
+  (make-string t)
+  (make-string-input-stream t)
+  (make-symbol t)
+  (make-synonym-stream t)
+  (make-two-way-stream t (1 2))
+  ((map map-into) t (3 -1 2 1))
+  ((mapcar mapcan maplist mapcon) t (2 -1 1))
+  ((min max) t (1 -1))
+  (merge t (2 3 1))
+  (merge-pathnames t (1 2))
+  (method-qualifiers nil)
+  ((minusp plusp zerop) t)
+  (mismatch t (1 2))
+  ((mod rem) t (1 2))
+  ; multiple-value-call (?...)
+  (name-char t)
+  ; no-applicable-method no-next-method
+  ; ((nreconc revappend) t ?)
+  ((reverse nreverse) t)
+  ((string-capitalize string-downcase string-upcase
+                      nstring-capitalize nstring-downcase nstring-upcase)
+   t)
   (nsublis 2) ((nsublis *) 1)
   (nsubst 3) (nsubst-if 3) (nsubst-if-not 3)
   (nsubstitute 3) (nsubstitute-if 3) (nsubstitute-if-not 3)
   (nth 2) ((nth *) 1)
   (nthcdr 2) ((nthcdr *) 1)
-  null
-  numberp
-  nunion ((nunion *) 2)
-  open
-  open-stream-p
-  output-stream-p
-  package-error-package
-  package-name
-  package-nicknames
-  package-shadowing-symbols
-  package-use-list
-  package-used-by
-  packagep
-  (pairlis 3)
-  parse-integer
-  parse-namestring
-  pathname
-  pathname-device
-  pathname-directory
-  pathname-host
-  pathname-match-p (pathname-match-p *)
-  pathname-name
-  pathname-type
-  pathname-version
-  pathnamep
-  phase
-  plusp
-  (position 2) (position-if 2) (position-if-not 2)
+  (null t)
+  (numberp t)
+  (open t)
+  (open-stream-p nil)
+  (package-error-package nil)
+  ((package-name
+    package-nicknames
+    package-shadowing-symbols
+    package-use-list
+    package-used-by-list)
+   nil)
+  (packagep t)
+  (pairlis t (3 1 2))
+  (parse-integer t) ; Ignore second return value.
+  (parse-namestring t) ; Ignore second return value.
+  ((pathname pathnamep) t)
+  ((pathname-device pathname-directory pathname-host
+                    pathname-name pathname-type pathname-version)
+   nil)
+  (pathname-match-p t (1 2))
   (pprint-dispatch 2) ((pprint-dispatch *) 1)
   (pprint-fill 2) (pprint-linear 2) (pprint-tabular 2)
   prin1 prin1-to-string
@@ -351,9 +318,7 @@
   readtable-case
   readtablep
   realp
-  realpart
   (reduce 2)
-  rem (rem *)
   (remhash 2) ((remhash *) 1)
   (remove 2) ((remove *) 1) (remove-if 2) (remove-if-not 2)
   remove-duplicates
@@ -362,8 +327,6 @@
   replace ((replace *) 2)
   rest
   restart-name
-  (revappend 2) ((revappend *) 1)
-  reverse
                                         ;(rotate &rest 0)
                                         ;(rotate* &rest 0)
                                         ;(shift &rest ?)
@@ -372,8 +335,6 @@
   scale-float (scale-float *)
   search ((search *) 2)
   set
-  set-difference ((set-difference *) 2)
-  set-exclusive-or ((set-exclusive-or *) 2)
   signum
   simple-bit-vector-p
   simple-condition-format-arguments
@@ -386,7 +347,6 @@
   slot-value (slot-value *)
   sort
   special-operator-p
-  sqrt
   stable-sort
   standard-char-p
   stream-element-type
@@ -394,8 +354,6 @@
   stream-external-format
   streamp
   string
-  string-capitalize
-  string-downcase
   string-equal ((string-equal *) 2)
   string-greaterp ((string-greaterp *) 2)
   (string-left-trim 2) ((string-left-trim *) 1)
@@ -405,7 +363,6 @@
   string-not-equal ((string-not-equal *) 2)
   string-not-greaterp ((string-not-greaterp *) 2)
   string-not-lessp ((string-not-lessp *) 2)
-  string-upcase
   string/= ((string/= *) 2)
   string< ((string< *) 2)
   string<= ((string<= *) 2)
@@ -455,5 +412,4 @@
   vectorp
   wild-pathname-p
   write
-  write-to-string
-  zerop)
+  write-to-string)
