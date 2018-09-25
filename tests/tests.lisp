786280a880e02bb56a5e60dc7a953181443c6538
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-09-25 09:53:32 -0400

    Tests: Use proper argument order for IS.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -6,89 +6,74 @@
 
 (define-test "featured-examples"
   ;;; 3 trivial examples
-  (is =
+  (is = 8
       (let ((place 7))
         (modify (1+ place))
-        place)
-      8)
-  (is equal
+        place))
+  (is equal '(new old)
       (let ((place '(old)))
         (modify (cons 'new place))
-        place)
-      '(new old))
-  (is equal
+        place))
+  (is equal '("hello" "hi")
       (let ((place '("hello" "hi")))
         (modify (adjoin "HELLO" place :test #'string-equal))
-        place)
-      '("hello" "hi"))
+        place))
 
   ;;; Equivalent to hundreds of modify macros!
-  (is equal
+  (is equal '(3 2 1)
       (let ((place (list 1 2 3)))
         (modify (nreverse place))
-        place)
-      '(3 2 1))
-  (is string=
+        place))
+  (is string= "YAY"
       (let ((place "Yay"))
         (modify (string-upcase place))
-        place)
-      "YAY")
-  (is equal
+        place))
+  (is equal '(atom)
       (let ((place 'atom))
         (modify (list place))
-        place)
-      '(atom))
-  (is eq
+        place))
+  (is eq (find-class 'symbol)
       (let ((place 'symbol))
         (modify (class-of place))
-        place)
-      (find-class 'symbol))
-  (is =
+        place))
+  (is = 1986
       (let ((place "1986"))
         (modify (parse-integer place))
-        place)
-      1986)
+        place))
 
   ;;; MODIFY return values, :old
-  (is equal
+  (is equal '(256 256)
       (let ((place 2))
         (list (modify (expt place 8))
-              place))
-      '(256 256))
-  (is equal
+              place)))
+  (is equal '(2 256)
       (let ((place 2))
         (list (modify (:old (expt place 8)))
-              place))
-      '(2 256))
+              place)))
 
   ;;; PME VS place
-  (is equalp
+  (is equalp '(e #((e)))
       (let ((object (vector 'e)))
         (list (modify (:old (list (aref object 0))))
-              object))
-      '(e #((e))))
-  (is equal
+              object)))
+  (is equal '(((d . 4))
+        (first a 1 B 2 (c . 3) (d . 4)))
       (let ((list '((d . 4))))
         (list (modify (:old (cons 'first (list* 'a 1 'b 2 (acons 'c 3 list)))))
-              list))
-      '(((d . 4))
-        (first a 1 B 2 (c . 3) (d . 4))))
-  (is equalp
+              list)))
+  (is equalp '(#(e) (e))
       (let ((object (vector 'e)))
         (list (modify (:old (list (aref (:place object) 0))))
-              object))
-      '(#(e) (e)))
-  (is equalp
+              object)))
+  (is equalp '(#(e) e)
       (let ((object (vector 'e)))
         (list (modify (:old (aref (:place object) 0)))
-              object))
-      '(#(e) e))
+              object)))
 
   ;; Multiple PMEs: setf-like
-  (is equal
+  (is equal '((a) (b a) b)
       (let ((x 'a) (y 'b))
         (list (modify (list x)
                       (:old (cons y x)))
               x
-              y))
-      '((a) (b a) b)))
+              y))))
